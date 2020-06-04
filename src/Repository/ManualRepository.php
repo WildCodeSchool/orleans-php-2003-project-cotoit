2f5ee10811a093bd80ffd98cc19d108f81e1bc09
Julien <eoz.wild@gmail.com>
2020-06-04 11:33:40 +0200

    Suppression code commenté auto-généré dans ManualRepository

--- src/Repository/ManualRepository.php
+++ src/Repository/ManualRepository.php
@@ -18,33 +18,4 @@ class ManualRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Manual::class);
     }
-
-    // /**
-    //  * @return Manual[] Returns an array of Manual objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Manual
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
