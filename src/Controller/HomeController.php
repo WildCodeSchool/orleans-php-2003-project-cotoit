c505822212308bd4800095c709ed492553421058
Julien <eoz.wild@gmail.com>
2020-06-04 09:58:06 +0200

    Fetch données entité Manual dans méthode Index de HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Manual;
 
 class HomeController extends AbstractController
 {
@@ -12,8 +13,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $manuals =  $this->getDoctrine()
+            ->getRepository(Manual::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'manuals' => $manuals,
         ]);
     }
 }
