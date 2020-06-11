2e84c2758d39fcb5351d6c162fd59ce108c90b0e
Julien <eoz.wild@gmail.com>
2020-06-11 14:29:23 +0200

    Modification style formulaire

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Activity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,15 +15,16 @@ class ActivityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('hours')
+            ->add('name', TextType::class, ['label' => 'Activité'])
+            ->add('hours', IntegerType::class, ['label' => 'Heures'])
             ->add('minutes', ChoiceType::class, [
                 'choices' => [
                     '00' => 00,
                     '15' => 15,
                     '30' => 30,
                     '45' => 45,
-                ]
+                ],
+                'label' => 'Minutes'
             ])
         ;
     }
