99abb897221a6b064db7d977d51433ad32a264fe
Julien <eoz.wild@gmail.com>
2020-06-11 09:39:23 +0200

    Création entité Activity avec ses asserts

---
+++ src/Entity/Activity.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ActivityRepository::class)
+ */
+class Activity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="L'activité doit comporter un nom")
+     * @Assert\Length(max="100", maxMessage="Le nom de l'actitivté doit comporter {{ limit }} caractères maximum")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="Le nombre d'heures allouées à l'activité  doit être définie")
+     */
+    private $hours;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $minutes;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHours(): ?int
+    {
+        return $this->hours;
+    }
+
+    public function setHours(int $hours): self
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+
+    public function getMinutes(): ?int
+    {
+        return $this->minutes;
+    }
+
+    public function setMinutes(int $minutes): self
+    {
+        $this->minutes = $minutes;
+
+        return $this;
+    }
+}
