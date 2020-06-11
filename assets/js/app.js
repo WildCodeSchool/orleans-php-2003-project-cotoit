227398a0e809c56f72ed406b688c29261531f6c1
heldeenn <amandine.ln7@gmail.com>
2020-06-11 15:26:22 +0200

    modification JS custom input

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,8 +12,8 @@ const $ = require('jquery');
 require('bootstrap');
 require('../scss/app.scss');
 
-$('.custom-file-input').on('change', function (event) {
-    var inputFile = event.currentTarget;
+$('.custom-file-input').on('change', (event) => {
+    const inputFile = event.currentTarget;
     $(inputFile).parent()
         .find('.custom-file-label')
         .html(inputFile.files[0].name);
