cf64a940eccbf62eb1d55c79c51d5ca9fcd66fe0
Julien <eoz.wild@gmail.com>
2020-06-29 15:43:26 +0200

    Ajout constantes fixtures avec données réelles

--- src/DataFixtures/ActivityFixtures.php
+++ src/DataFixtures/ActivityFixtures.php
@@ -10,11 +10,55 @@ use Faker;
 
 class ActivityFixtures extends Fixture
 {
-    const MINUTES = [
-        00,
-        15,
-        30,
-        45,
+    const ACTIVITIES = [
+        'Visite' => [
+            'hours' => 2,
+            'minutes' => 30,
+        ],
+        'Immeuble de moins de 2 ans' => [
+            'hours' => 10,
+            'minutes' => 0,
+        ],
+        'Réunion du conseil syndical' => [
+            'hours' => 4,
+            'minutes' => 0,
+        ],
+        'Tenue assemblée générale' => [
+            'hours' => 3,
+            'minutes' => 0,
+        ],
+        'Résolution travaux' => [
+            'hours' => 1,
+            'minutes' => 0,
+        ],
+        'Ordre de service' => [
+            'hours' => 0,
+            'minutes' => 30,
+        ],
+        'Vente' => [
+            'hours' => 0,
+            'minutes' => 30,
+        ],
+        'Relance' => [
+            'hours' => 0,
+            'minutes' => 1,
+        ],
+        'Sinistre MRH' => [
+            'hours' => 2,
+            'minutes' => 0,
+        ],
+        'Sinistre DO' => [
+            'hours' => 2,
+            'minutes' => 0,
+        ],
+        'Expertise avec présence gestionnaire' => [
+            'hours' => 1,
+            'minutes' => 30,
+        ],
+        'Suivi et transmission avocat/huissier dossier contentieux' => [
+            'hours' => 1,
+            'minutes' => 0,
+        ],
     ];
 
     public function load(ObjectManager $manager)
