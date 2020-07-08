9be2c331c7996d529c455cdc1a642b21d23394f8
heldeenn <amandine.ln7@gmail.com>
2020-07-08 11:44:22 +0200

    ajout controller et type pour le taux horaire

---
+++ src/Form/AdminHourlyRateType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\HourlyRate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminHourlyRateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('rate', TextType::class, [
+                'label' => 'Taux horaire',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => HourlyRate::class,
+        ]);
+    }
+}
