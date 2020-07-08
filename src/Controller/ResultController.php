e62104f81868c1dc8d1c070a1509a25dd7b75966
maoherve <maoherve8@gmail.com>
2020-07-08 13:56:04 +0200

    debut export result to pdf

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\CalculatingManager;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**
  * @Route("/result", name="result_")
@@ -39,5 +41,31 @@ class ResultController extends AbstractController
             'topTenCondos' => $topTenCondos,
             'nonProfitableCondos' => $nonProfitableCondos,
         ]);
+    }
+
+    /**
+     * @Route("/pdf", name="pdf")
+     */
+    public function resultpdf()
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('result/pdfResult.html.twig', [
+            'title' => "Welcome to our PDF Test"
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream();
     }
 }
